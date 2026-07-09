767256cd8a00136e7816bd827759651272b1e972
Sam Kennard <s13k@live.com>
2026-07-09 13:05:16 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,127 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <section id="body">
+      <section id="center">
+        <div id='flavor-text'>　**　°　°　✦✦　　✯　　　　　　✵　　　　★　　　*　✯　　✷　•　　°　 **　　　　　　　**°　　✦　　　*　•　　　·°✸　　　　　°✶　　　　·　 •.*　*　.　　　　•　　　　　　　　　　✷　　　　　　　　　.　　　　　　　 °°•✯　　　　　·•°　　　•　　·　·.　　•　　　　　　•　✧　　　　　　 　✷　°　.°　　•　　　.　　　*　　　*.·　　　　　.　　　　　　　*.　 　　　°　　　　　✸　　　　　　.　　　　　　　　　　✦**　.°　*　　.　* 　·　　　　　　　　　.•✦*　　.　　*　　　　　　　　　　　　　°　　.　· 　　　　°　　　　·.　•*　　　　　　　　　✯　　　　　　　　✶　•·　　　. 　　·　°　　　•　　*·°　　·　　　　　　*　·　　　　　　　　　°✷°✶　 　 　　　　　•　　　　*　°　•　　　✷　　　　•　　.　　　　　　　　　°　•　 　°　　　　　　*　　　　　　　.　•　　　　　·✵·.°　✯　　.　　　　★　 　　　.✯•　　　　　.　　　　*.　°•　·　　　★　　　*　　　　　　·　　 **　✧　*　　　　✵　　　　***•　　·　.　*　　　　·.　　　　✶　·* 　　　　·　•　　　*　　　　　　✧　✦　　°　　　　　　　　.　°　*　　✯* 　　　.　　✵　✷　*　　°　*　　..　*　　　　　　　　..　　　　　　　　 　　*　.　　　.　✷　°　　　　　　　°　•　·　*　　　　★　.☆•　✯　. 　　★*.•.　*　　✯*　　·　　　　　☆　　　　　　°　　　　.　　　°　✶　**　°　°　✦✦　　✯　　　　　　✵　　　　★　　　*　✯　　✷　•　　°　 **　　　　　　　**°　　✦　　　*　•　　　·°✸　　　　　°✶　　　　·　 •.*　*　.　　　　•　　　　　　　　　　✷　　　　　　　　　.　　　　　　　 　　　　　　　•·•••*　　 °°•✯　　　　　·•°　　　•　　·　·.　　•　　　　　　•　✧　　　　　　 　✷　°　.°　　•　　　.　　　*　　　*.·　　　　　.　　　　　　　*.　 　　　°　　　　　✸　　　　　　.　　　　　　　　　　✦**　.°　*　　.　* 　·　　　　　　　　　.•✦*　　.　　*　　　　　　　　　　　　　°　　.　· 　　　　°　　　　·.　•*　　　　　　　　　✯　　　　　　　　✶　•·　　　. 　　·　°</div>
+        <div id="header-box">
+          <h1>sam kennard</h1>
+        </div>
+      </section>
+      <section id="content">
+        <div className="title">creative engineer</div>
+      <div id="item">
+        <div id="item-child">about me <br/><br/>
+         i am a multidisciplined creative. despite working primarily as a software engineer at google, i have spent most of my life creating in some regard. below are some highlights of the things i have created. from directing, shooting, editing, colour grading short films to stand up comedy, to music and back to programming, i have sampled almost every creative field out there. <br/><br/><br/> i am also a keen photographer, writer and 3D artist, as well as teaching myself electrical engineering skills in order to set a world record for building the world's smallest GameBoy.
+        </div>
+      </div>
+        Directing
+        <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>james longcake - painted nails</h1>
+            <iframe className="responsive-iframe" width="560" height="315" src="https://www.youtube.com/embed/qvPxy3Qig3U?si=fA_sPdFLEPdFtG1Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <p>i directed, shot, edited this music video with a small team for the artist james longcake. this video organically received more than 140, 000 views on youtube.</p>
+          </div>
+        </div>
+        <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>ldn - short film</h1>
+            <iframe className="responsive-iframe" width="560" height="315" src="https://www.youtube.com/embed/1v7OB4tGke0?si=bnYWsFU3rX43mLzm" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <p>i wrote, shot and edited this short film with the intention of submitting it to film festivals.</p>
+          </div>
+        </div>
+        <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>oxygen - short film</h1>
+            <iframe className="responsive-iframe" width="560" height="315" src="https://www.youtube.com/embed/Obrs5sqm3e0?si=P3oPqE2di2Z3QfVl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <p>i directed, shot and edited this multi-award winning short film, which picked up nominations for NaSTA awards, and won awards at both Brighton Rocks Film Festival and Cinecity 2019.</p>
+          </div>
+        </div>
+        Creative programming
+        <div id="item">
+          <div id="item-child">
+            <iframe className="shorts" width="315" height="560" src="https://www.youtube.com/embed/SYJvfoVP1Kk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <h1>gesture controlled camera</h1>
+            <p>built with python, i created a way of taking pictures using hand tracking and gesture controls.</p>
+          </div>
+        </div>
+        <div id="item">
+          <div id="item-child">
+            <h1>custom video renderer</h1>
+            <p>i built a way of rendering video using images. using image luminance as a key for pixel size, i was able to create dynamic and engaging visuals using ASCII characters. <br/> <br/> this was developed as part of an upcoming music video project for a popular jazz musician.</p>
+          </div>
+          <div id="item-child">
+            <iframe className="shorts" width="315" height="560" src="https://www.youtube.com/embed/0wwSq5tX8WM?si=TgmgnDWYrNOLuTc9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div>
+        <div id="item">
+          <div id="item-child">
+            <iframe className="shorts" width="315" height="560" src="https://www.youtube.com/embed/8enAzlzhsjA?si=BX1DVSH8KFEHLoNh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <h1>starling simulation</h1>
+            <p>using Unity, i simulated the behaviour of a starling murmuration by applying the "boids" flocking algorithm</p>
+          </div>
+        </div>
+        <div id="item">
+          <div id="item-child">
+            <h1>musical physics</h1>
+            <p>by applying my knowledge of music theory to some simple physics rules, i was able to create music generated by movement.</p>
+          </div>
+          <div id="item-child">
+          <iframe className="shorts" width="315" height="560" src="https://www.youtube.com/embed/MbKHxNF2HIQ?si=n58fqzVnceyHmn8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        </div>
+         Music
+        <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>khama road club - watercolour barbeque</h1>
+          <iframe width="100%" height="166" scrolling="no"
+            frameborder="no" allow="autoplay; encrypted-media"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2049132868%3Fsecret_token%3Ds-6edJq6Osu60&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
+          </iframe>
+          <div className="soundcloud-embed">
+            <a title="khama road club" target="_blank" className="soundcloud-embed-text">watercolour barbeque</a>
+            </div>
+          </div>
+          <div id="item-child">
+            <p>i occasionally sing in a band where we make disgustingly sincere indie tunes.</p>
+          </div>
+        </div>
+        <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>glass animals interview</h1>
+            <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A1867058202%3Fsecret_token%3Ds-6Fo1KKXnTuJ&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div className="soundcloud-embed"><a href="https://soundcloud.com/user-611114974" title="Eskay" target="_blank" className="soundcloud-embed-text">Eskay</a> · <a href="https://soundcloud.com/user-611114974/a-little-something/s-6Fo1KKXnTuJ" title="a little something" target="_blank" className="soundcloud-embed-text">a little something</a></div>
+          </div>
+          <div id="item-child">
+            <p>back in 2016 when i was persuing music journalism on the side, i interviewed mercury prize nominated, grammy award nominated and brit award nominated band glass animals.</p>
+          </div>
+        </div>
+        Stand up comedy
+         <div id="item" className="vertical">
+          <div id="item-child">
+            <h1>the wikihow writer</h1>
+            <iframe className="responsive-iframe" width="560" height="315" src="https://www.youtube.com/embed/91ynV_xsvfM?si=MDJL2ZKscjn4HpOd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+          <div id="item-child">
+            <p>for a while i persued stand up comedy, until eventually getting my current role at Google, which prevented me from having the time to persue it further.</p>
+          </div>
+        </div>
+      </section>
+    </section>
+    </>
+
+  )
+}
+
+export default App
